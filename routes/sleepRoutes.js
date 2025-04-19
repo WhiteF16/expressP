9cc99ea801b2ad3c29c1c44ece1dd37b8b644688
WhiteF16 <1435904264@qq.com>
2025-04-19 23:02:40 +0800

    updatebug

--- routes/sleepRoutes.js
+++ routes/sleepRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const combinedMiddleware=require('../middlewares/multerMiddleware')
+const sleepController=require('../controllers/sleepController')
 const router = express.Router();
 
 router.post('/sleepData',combinedMiddleware,sleepController.sleepScore);
